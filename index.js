37778849fa42923ca2593629157098818a32aa6c
chenzhe <chenzhe@meicai.cn>
2018-12-31 12:06:56 +0800

    nothing

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+import React, {Component} from 'react';
+import {View, Text, Image} from 'react-native';
 
-import { NativeModules } from 'react-native';
+export default class CZImagePlaceholder extends Component{
 
-const { RNCzImagePlaceholder } = NativeModules;
-
-export default RNCzImagePlaceholder;
+    render() {
+        return (
+            <View>
+                <Text>1111</Text>
+                <Image source={require('./images/wechat_icon.png')} style={{width: 50, height: 50}}/>
+            </View>
+        )
+    }
+}
